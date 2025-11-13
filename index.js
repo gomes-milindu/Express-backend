cfc6251c1291074efb45561ca2f3c4864527d665
gomes-milindu <gomesmilindu@gmail.com>
2025-11-13 22:47:01 +0530

    database integrated

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello")
+import express from "express"
+import mongoose from "mongoose"
+
+const connectionString = "mongodb+srv://backend-clone:1234@cluster0.arkyzdz.mongodb.net/?appName=Cluster0"
+
+const app = express()
+app.use(5000,success)
+
+function success(){
+    console.log("Succes is working")
+}
+
+mongoose.connect(connectionString).then(
+    ()=>{
+        console.log("Database Connected")
+    }
+).catch(
+    ()=>{
+        console.log("DataBase Not Connected")
+    }
+)
